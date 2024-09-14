9d237e7093509ccf30a017c19beb667d73580ae1
Stefano Bragg <70184158+Braggstefano@users.noreply.github.com>
2024-09-14 17:11:30 +0800

    Delete Task4 directory

--- Task4/case_retrieve/baseline/1_indexer.sh
+++
@@ -1,8 +0,0 @@
-python -m pyserini.index.lucene \
-  --collection JsonCollection \
-  --input workplace/CAIL_Task4/data/documents \
-  --index workplace/CAIL_Task4/data/index \
-  --generator DefaultLuceneDocumentGenerator \
-  --threads 1 \
-  --storePositions --storeDocvectors --storeRaw \
-  --stopwords workplace/CAIL_Task4/data/others/stopword.txt
