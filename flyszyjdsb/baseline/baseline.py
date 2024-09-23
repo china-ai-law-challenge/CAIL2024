e5d36a972ecea13466b29b8f3416ba3bb983c828
pygong <pygongnlp@gmail.com>
2024-09-24 02:54:51 +0800

    modify

--- flyszyjdsb/baseline/baseline.py
+++ flyszyjdsb/baseline/baseline.py
@@ -1,9 +1,10 @@
-from zhipuai import ZhipuAI
-from tqdm import tqdm
 import argparse
 import os
+import json
+from tqdm import tqdm
+from zhipuai import ZhipuAI
 
-client = ZhipuAI(api_key="")  # 输入GLM4 API Key
+client = ZhipuAI(api_key="") # 请自行填写
 
 
 def generate(model_name, prompt):
@@ -19,15 +20,25 @@ def generate(model_name, prompt):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--dataset_path", type=str, default="dataset")
+    parser.add_argument("-d", "--dataset_path", type=str, required=True)
+    parser.add_argument("-o", "--output_path", type=str, required=True)
     parser.add_argument("-p", "--prompt_path", type=str, default="prompts")
-    parser.add_argument("-m", "--model_name", type=str, default="GLM-4")
+    parser.add_argument("-m", "--model_name", type=str, default="GLM-4-FLASH")
     args = parser.parse_args()
 
+    results = []
     for file_path in tqdm(os.listdir(args.dataset_path)):
+        file_name = file_path.strip(".txt")
         file_path = os.path.join(args.dataset_path, file_path)
         conv = open(file_path, 'r', encoding='utf8').read().strip('\n')
 
+        # extract laws
+        prompt_path = os.path.join(args.prompt_path, 'extract_laws.txt')
+        prompt = open(prompt_path, 'r', encoding='utf8').read()
+        prompt = prompt.replace('[conv]', conv)
+        laws = generate(args.model_name, prompt)
+
+        # generate points
         prompt_path = os.path.join(args.prompt_path, 'generate_points.txt')
         prompt = open(prompt_path, 'r', encoding='utf8').read()
         prompt = prompt.replace('[conv]', conv)
@@ -35,4 +46,17 @@ if __name__ == '__main__':
         point = point.lstrip("争议焦点：").strip("\n")
         if len(point.split("\n")) != 1 and "争议焦点" in point.split("\n")[0]:
             point = "".join(point.split("\n")[1:])
-        print(point)
+
+        results.append({
+            "id": file_name,
+            "legal_elements": laws.split("\n"),
+            "dispute_focus": point
+        })
+
+    json.dump(results, open(args.output_path, "w", encoding="utf8"), ensure_ascii=False, indent=4)
+
+
+
+
+
+
--- flyszyjdsb/baseline/baseline_withcause.py
+++ flyszyjdsb/baseline/baseline_withcause.py
@@ -1,11 +1,12 @@
-from zhipuai import ZhipuAI
-from tqdm import tqdm
 import argparse
 import os
+import json
+from tqdm import tqdm
+from zhipuai import ZhipuAI
 
-client = ZhipuAI(api_key="")  # 输入GLM4 API Key
+client = ZhipuAI(api_key="") # 请自行填写
 
-cause_list = ["信用卡纠纷", "知识产权纠纷", "买卖合同纠纷", "服务合同纠纷", "物业合同纠纷"]
+cause_list = ["信用卡纠纷", "房产租赁合同纠纷", "买卖合同纠纷", "服务合同纠纷", "物业合同纠纷"]
 
 
 def generate(model_name, prompt):
@@ -21,12 +22,15 @@ def generate(model_name, prompt):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--dataset_path", type=str, default="dataset")
+    parser.add_argument("-d", "--dataset_path", type=str, required=True)
+    parser.add_argument("-o", "--output_path", type=str, required=True)
     parser.add_argument("-p", "--prompt_path", type=str, default="prompts")
-    parser.add_argument("-m", "--model_name", type=str, default="GLM-4")
+    parser.add_argument("-m", "--model_name", type=str, default="GLM-4-FLASH")
     args = parser.parse_args()
 
+    results = []
     for file_path in tqdm(os.listdir(args.dataset_path)):
+        file_name = file_path.strip(".txt")
         file_path = os.path.join(args.dataset_path, file_path)
         conv = open(file_path, 'r', encoding='utf8').read().strip('\n')
 
@@ -35,9 +39,13 @@ if __name__ == '__main__':
         cause_prompt = cause_prompt.replace('[conv]', conv)
         cause_text = generate(args.model_name, cause_prompt)
         matched_causes = [cause for cause in cause_list if cause in cause_text]
-        if len(matched_causes) != 1: continue
         cause = matched_causes[0]
-        print(cause)
+
+        prompt_path = os.path.join(args.prompt_path, 'extract_laws_withcause.txt')
+        prompt = open(prompt_path, 'r', encoding='utf8').read()
+        prompt = prompt.replace('[conv]', conv)
+        prompt = prompt.replace('[cause]', cause)
+        laws = generate(args.model_name, prompt)
 
         prompt_path = os.path.join(args.prompt_path, 'generate_points_withcause.txt')
         prompt = open(prompt_path, 'r', encoding='utf8').read()
@@ -47,4 +55,11 @@ if __name__ == '__main__':
         point = point.lstrip("争议焦点：").strip("\n")
         if len(point.split("\n")) != 1 and "争议焦点" in point.split("\n")[0]:
             point = "".join(point.split("\n")[1:])
-        print(point)
+
+        results.append({
+            "id": file_name,
+            "legal_elements": laws.split("\n"),
+            "dispute_focus": point
+        })
+
+    json.dump(results, open(args.output_path, "w", encoding="utf8"), ensure_ascii=False, indent=4)
